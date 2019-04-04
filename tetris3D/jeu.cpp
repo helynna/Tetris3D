4a8c051b8c8cb523695618c67b02a3aea9976d02
Helynna <elodie.charitat@gmail.com>
2019-04-04 08:28:13 +0200

    Unfinished condition for going down

--- tetris3D/jeu.cpp
+++ tetris3D/jeu.cpp
@@ -95,15 +95,25 @@ bool Jeu::peutDescendre(std::vector<std::vector<int>> shape,int pos[2])
     else{//On est pas à la dernière ligne
         for(int j=0;j<shape.size();j++){
             if(shape[shape.front().size()-1][j]==1){
-                if(){
-                   return false;
+                if((board_->getIndiceGrille(pos[0]+1,pos[1]+j))!=7){
+                   res=false;
                 }
+                else{
+                    res=true;
+                }
+
                 //Ici il faut prendre la dernière case du shape
                 //qu'on ajoute à la pos actuelle, on verifie s'il y a des 1
                 //si oui on verifie que dessous c'est libre
                 //puis on verifie que les cases au dessus sont libres
                 //surtout si la derniere est vide
                 //pos[0]+i,pos[1]+j)
+            }
+            else{//Notre dernière case de la pièce ne contient pas de 1, donc pas de pièce
+                if(shape.front().size()){
+
+                }//blbl
+
             }
         }
     }
